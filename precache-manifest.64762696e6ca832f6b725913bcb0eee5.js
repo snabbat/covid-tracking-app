38645d5ff11a35f0ed0bf08279969e765dad2dd1
soufiane <nabbat.soufiane@gmail.com>
2020-05-10 03:55:44 +0000

    Updates

---
+++ precache-manifest.64762696e6ca832f6b725913bcb0eee5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ae7f01956f708a356e1832c58c2e62",
+    "url": "/covid-tracking-app/index.html"
+  },
+  {
+    "revision": "50e581bccb09bfc24110",
+    "url": "/covid-tracking-app/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "5fcad1a1a56e08c99536",
+    "url": "/covid-tracking-app/static/css/main.9ea8e0da.chunk.css"
+  },
+  {
+    "revision": "50e581bccb09bfc24110",
+    "url": "/covid-tracking-app/static/js/2.1a7e7deb.chunk.js"
+  },
+  {
+    "revision": "944bbb627520378f816db0226ba7bf9e",
+    "url": "/covid-tracking-app/static/js/2.1a7e7deb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fcad1a1a56e08c99536",
+    "url": "/covid-tracking-app/static/js/main.69d5527f.chunk.js"
+  },
+  {
+    "revision": "c2c338b1838278cdf649",
+    "url": "/covid-tracking-app/static/js/runtime-main.a29a04a3.js"
+  }
+]);
